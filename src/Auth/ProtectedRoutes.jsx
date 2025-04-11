1b026accda78405e2f1f2bd453adef218a04d241
Pramod Mahto <Mahtop324@gmail.com>
2025-04-11 08:08:07 +0545

    added google login

--- src/Auth/ProtectedRoutes.jsx
+++ src/Auth/ProtectedRoutes.jsx
@@ -1,12 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ProtectedRoutes({ components }) {
   const {  isAuthenticated } = useAuth0();
   const navigate = useNavigate();
+ useEffect(()=>{
   if (!isAuthenticated) {
     return navigate("/login");
   }
+ },[])
 
   return components;
 }
